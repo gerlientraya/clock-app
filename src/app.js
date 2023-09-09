5abcb25f9f608d29a6e0c7e10f453cd61488c119
gerlientraya <gntraya@gmail.com>
2023-09-09 22:36:23 +0200

    adding some javascript functionality to the app for testing!

---
+++ src/app.js
@@ -0,0 +1,18 @@
+function showTime(event){
+  if(event.target.value.length > 0){
+    if(event.target.value === "philippines"){
+      alert("Magandang Umaga!")
+    }
+    if(event.target.value === "spain"){
+      alert("Buenas Dias!")
+    }
+    if(event.target.value === "lisbon"){
+      alert("Bom Dia!")
+    }
+  }
+}
+
+
+
+let selectedCountry = document.querySelector("#cities");
+selectedCountry.addEventListener("change", showTime)
