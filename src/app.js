82bbd463afa1ebc8b2a97fbf2f59e709d762a4af
gerlientraya <gntraya@gmail.com>
2023-09-10 20:55:59 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-function showTime(event){
-  if(event.target.value.length > 0){
-    if(event.target.value === "philippines"){
-      alert("Magandang Umaga!")
+function showTime(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value === "philippines") {
+      alert("Magandang Umaga!");
     }
-    if(event.target.value === "spain"){
-      alert("Buenas Dias!")
+    if (event.target.value === "spain") {
+      alert("Buenas Dias!");
     }
-    if(event.target.value === "lisbon"){
-      alert("Bom Dia!")
+    if (event.target.value === "lisbon") {
+      alert("Bom Dia!");
     }
   }
 }
 
-
-
 let selectedCountry = document.querySelector("#cities");
-selectedCountry.addEventListener("change", showTime)
+selectedCountry.addEventListener("change", showTime);
