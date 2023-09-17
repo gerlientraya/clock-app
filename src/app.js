e98b06151c7282533207a85ee7c19ba8086352d9
gerlientraya <gntraya@gmail.com>
2023-09-17 18:40:08 +0200

    change the hard code into real time data

--- src/app.js
+++ src/app.js
@@ -12,5 +12,27 @@ function showTime(event) {
   }
 }
 
-let selectedCountry = document.querySelector("#cities");
-selectedCountry.addEventListener("change", showTime);
+//show data of copenhagen
+function updateTime() {
+  let copenhagenElement = document.querySelector("#copenhagen");
+  let copenhagenDateElement = copenhagenElement.querySelector(".date");
+  let copenhagenTimeElement = copenhagenElement.querySelector(".time");
+  copenhagenDateElement.innerHTML = moment()
+    .tz("Europe/Copenhagen")
+    .format("MMMM D, YYYY ");
+  copenhagenTimeElement.innerHTML = moment()
+    .tz("Europe/Copenhagen")
+    .format("hh:mm:ss [<small>]A[</small>]");
+  //show Data of singapore
+  let singaporeElement = document.querySelector("#singapore");
+  let singaporeDateElement = singaporeElement.querySelector(".date");
+  let singaporeTimeElement = singaporeElement.querySelector(".time");
+  singaporeDateElement.innerHTML = moment()
+    .tz("Asia/Singapore")
+    .format("MMMM D, YYYY");
+  singaporeTimeElement.innerHTML = moment()
+    .tz("Asia/Singapore")
+    .format("hh:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
